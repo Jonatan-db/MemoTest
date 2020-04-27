f0eafa6b3b124480dc44675c086a3ffb5e824ef0
Jonatandb <jonatan.bonet@hasar.com>
2020-04-27 17:01:13 -0300

    Add files via upload

---
+++ precache-manifest.adb22bcb0261c7f83a01e43220076f40.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ac714b484eb43242dc2a16b46a2df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0f6e4fdb2281d78a839",
+    "url": "/static/css/2.3fad095e.chunk.css"
+  },
+  {
+    "revision": "24e114747b8500acca4d",
+    "url": "/static/css/main.92abf971.chunk.css"
+  },
+  {
+    "revision": "d0f6e4fdb2281d78a839",
+    "url": "/static/js/2.03fafcf2.chunk.js"
+  },
+  {
+    "revision": "24e114747b8500acca4d",
+    "url": "/static/js/main.57b8f36a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
